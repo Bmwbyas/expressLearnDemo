906cdf3312902571c5b3432f5c8bc73acf175f29
alexAskerko <askerko50@gmail.com>
2023-02-19 00:36:59 +0300

    middleware

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express from 'express'
+import express, {NextFunction, Request, Response} from 'express'
 import bodyParser from "body-parser";
 import {productsRoute} from "./routes/products-route";
 import {addressesRouter} from "./routes/addresses-routes";
@@ -7,15 +7,39 @@ import {addressesRouter} from "./routes/addresses-routes";
 const app = express()
 const port = process.env.PORT || 5000
 
+const blablaMiddleware = ((req: Request, res: Response, next: NextFunction) => {
+    // @ts-ignore
+    req.blabla = "hello"
+    next()
+})
+const authGuardMiddleware = ((req: Request, res: Response, next: NextFunction) => {
+    if (req.query.token === '123') {
+        next()
+    } else {
+        res.send(401)
+    }
+})
+let requestCounter = 0
+const requestCounterMiddleware = ((req: Request, res: Response, next: NextFunction) => {
+    requestCounter++;
+    next();
+})
+// app.use(requestCounterMiddleware)
+// app.use(blablaMiddleware)
+// app.use(authGuardMiddleware)
 
 
-
-const parserMiddleware=bodyParser({})
+const parserMiddleware = bodyParser({})
 app.use(parserMiddleware)
-app.use('/products',productsRoute)
-app.use('/addresses',addressesRouter)
+app.use('/products', productsRoute)
+app.use('/addresses', addressesRouter)
 //get
+app.get('/users', (req: Request, res: Response) => {
+// @ts-ignore
+    const blabla = req.blabla
 
+    res.send({value: blabla + '!!!!!',count:requestCounter})
+})
 
 //start app
 app.listen(port, () => {
